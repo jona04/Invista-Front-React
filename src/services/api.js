a3c53e3d1145850978aae6c7d5b9f583ae08de20
jonatasilva <jonatas.iw@gmail.com>
2020-05-14 12:51:28 -0300

    adicionar login no inicio do projeto e adicionar swatchboot

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    // baseUrlInvista: 'https://invistasite.herokuapp.com/api',
-    baseURL: 'http://127.0.0.1:8000/api',
+    baseURL: 'https://invistasite.herokuapp.com/api',
+    // baseURL: 'http://127.0.0.1:8000/api',
 });
 
 export default api;
