05ab65936e78acf98485f52cfead0c17b73871bb
jonatasilva <jonatas.iw@gmail.com>
2020-05-14 16:18:37 -0300

    atualizar url axios

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     baseURL: 'https://invistasite.herokuapp.com/api',
-    // baseURL: 'http://127.0.0.1:8000/api',
+    // baseURLe: 'http://127.0.0.1:8000/api',
 });
 
 export default api;
